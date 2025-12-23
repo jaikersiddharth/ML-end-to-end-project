936bed5518cf34d06e56c3446c40c5249f4bc9c2
jaikersiddharth <jaikersiddharth@email.com>
2025-12-23 15:52:15 +0530

    added model_trainer

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import dill
 import os
+from sklearn.metrics import r2_score
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -11,4 +12,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise e
 
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predict on test data
+            y_test_pred = model.predict(X_test)
+
+            # Calculate r2 score
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise e
                 
